bfb0a238ec5b6242f13073e22eae4566115b4104
Giovanni Delacqua <giovannidelacqua@lm3451.local>
2015-10-21 10:29:16 -0500

    firebase added

---
+++ www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var travel = angular.module('starter', ['ionic','firebase']);
+
+// travel.run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//     // for form inputs)
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// });
+
+//factory
+travel.factory('Items', ['$firebaseArray', function($firebaseArray){
+  var itemsRef = new Firebase('https://radiant-torch-278.firebaseio.com');
+  return $firebaseArray(itemsRef);
+}]);
+
+//controller
+travel.controller('ListCtrl', function($scope, Items) {
+
+  $scope.items = Items;
+
+  Items.$loaded(function(){
+    console.log('categories loaded from database!');
+  });
+
+  $scope.addItem = function(){
+    var name = prompt('Please insert a new Category');
+    if(name){
+      $scope.items.$add({
+        'name':name
+      });
+    }
+  };
+
+  $scope.deleteItem = function(item){
+    console.log(item.$id);
+    $scope.items
+      .$remove(item)
+      .then(function(){
+        alert('category deleted!')
+      });
+  };
+
+});
